881a45ccc11a0278221a798524c8a6bdfab4a89b
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-02-10 23:57:32 +0300

    commit
    commit

---
+++ 04_block_scope/script.js
@@ -0,0 +1,47 @@
+const x = 100;
+const foo = 1;
+var bar = 2;
+let led = 3;
+// var will be added to 'window' object, but let and const will not be added
+
+if (true) {
+  // console.log(x);
+  const y = 200;
+  console.log(x + y);
+}
+
+// y is not defined on global scope
+// console.log(x + y);
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// 'i' will be undefined because 'let' variables are not accesible from outside of block scope.
+console.log(i);
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// 'i' will be "11" because 'var' variables are accesible from outside of block scope.
+console.log(i);
+
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+// console.log(a); // error
+// console.log(b); // error
+console.log(c); // the big difference between 'var' and 'let', is,  var is accessible from outside. var is not block scoped, it is function scoped
+
+function run() {
+  var d = 100;
+  console.log(d);
+}
+
+run();
+
+console.log(d); // this will give us error even variable is defined as 'var' because var is function scoped so we cant access it from outside of the function
